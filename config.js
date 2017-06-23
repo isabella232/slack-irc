d497e6c8d2a851fd6ba731e5e88e6dfb2e6995df
Matteo Depalo <matteodepalo@gmail.com>
2017-06-23 11:34:26 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+        "nickname": "ircbot",
+        "server": "chat.freenode.net",
+        "token": process.env.SLACK_TOKEN,
+        "channelMapping": {
+                "#old-school": "#hackatron"
+        }
+}
